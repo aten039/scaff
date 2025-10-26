d86d35ecba48aacc087b5ceca1fe8e8a060be055
Elvis Linares <elvis.039@hotmail.com>
2025-10-26 18:35:27 -0400

    msg for javascript menu

--- internal/application/javascript/jspoo.go
+++ internal/application/javascript/jspoo.go
@@ -3,13 +3,14 @@ package javascript
 import (
 	"aten039/scaff/internal/components/menu"
 	"aten039/scaff/internal/msg"
+	"aten039/scaff/internal/scripts"
 )
 
 var jsPooOptions = []menu.OptionsInfo{
-	{Description: "Clean Arquitecture", Msg: msg.ChangeToParadigmMsg{}},
-	{Description: "Hexagonal Arquitecture", Msg: msg.ChangeToParadigmMsg{}},
-	{Description: "Model View Controller", Msg: msg.ChangeToParadigmMsg{}},
-	{Description: "Modular Clean", Msg: msg.ChangeToParadigmMsg{}},
+	{Description: "Clean Arquitecture", Msg: msg.RunningScriptMsg{Script: scripts.JsPooClean}},
+	{Description: "Hexagonal Arquitecture", Msg: msg.RunningScriptMsg{Script: scripts.JsPooHexa}},
+	{Description: "Model View Controller", Msg: msg.RunningScriptMsg{Script: scripts.JsPooMvc}},
+	{Description: "Modular Clean", Msg: msg.RunningScriptMsg{Script: scripts.JsPooMc}},
 }
 
 func InitialJsPooModel() menu.Model {
